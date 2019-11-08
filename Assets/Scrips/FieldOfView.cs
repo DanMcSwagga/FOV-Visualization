6c19d09d2e465c3e1550a81df9be14e1251d6476
Dan Stetsenko <denyast@ukr.net>
2019-11-08 23:57:13 +0200

    Add gizmos/debug FOV

--- Assets/Scrips/FieldOfView.cs
+++ Assets/Scrips/FieldOfView.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldOfView : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class FieldOfView : MonoBehaviour {
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask obstacleMask;
+    public LayerMask targetMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Start() {
+        StartCoroutine(FindTargetsWithDelay(.2f));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator FindTargetsWithDelay(float delay) {
+        while (true) {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets() {
+        visibleTargets.Clear();
+
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        // TODO: rework for in
+        foreach (Collider targetCollider in targetsInViewRadius) {
+            Transform target = targetCollider.transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            // If target is within our viewAngle
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                // If there are NO obstacles in the way
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool isGlobalAngle) {
+        if (!isGlobalAngle) {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
